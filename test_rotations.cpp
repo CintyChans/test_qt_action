2acb6095467348af0f76927dfc53fa5cdc221029
CintyChans <1046855257@qq.com>
2023-03-01 16:43:04 +0800

    -

--- test_rotations.cpp
+++ test_rotations.cpp
@@ -19,14 +19,6 @@ void TestRotations::toggleSunButton()
     Q3DScatter *graph = new Q3DScatter();
     QWidget *container = QWidget::createWindowContainer(graph);
 
-    if (!graph->hasContext())
-    {
-        QMessageBox msgBox;
-        msgBox.setText("Couldn't initialize the OpenGL context.");
-        msgBox.exec();
-        return -1;
-    }
-
     QWidget *widget = new QWidget;
     QHBoxLayout *hLayout = new QHBoxLayout(widget);
     hLayout->addWidget(container, 1);
@@ -36,7 +28,7 @@ void TestRotations::toggleSunButton()
     QObject::connect(toggleSunButton, &QPushButton::clicked, modifier,
                      &ScatterDataModifier::toggleSun);
 
-
+    QTest::mouseClick(&toggleSunButton);
 
 }
 
