a5245870767a12e12e98203b36df8441d0e47596
CintyChans <1046855257@qq.com>
2023-03-01 16:01:35 +0800

    - cmake

--- test_rotations.cpp
+++ test_rotations.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
-//! [0]
+
 #include <QTest>
 
 class TestQString: public QObject
@@ -10,17 +10,12 @@ class TestQString: public QObject
 private slots:
     void toUpper();
 };
-//! [0]
 
-//! [1]
 void TestQString::toUpper()
 {
     QString str = "Hello";
     QCOMPARE(str.toUpper(), QString("HELLO"));
 }
-//! [1]
 
-//! [2]
 QTEST_MAIN(TestQString)
 #include "testqstring.moc"
-//! [2]
